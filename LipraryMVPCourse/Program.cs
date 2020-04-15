0fe2065328cecee982344e2b35d59a89214fa928
Your Name <you@example.com>
2020-04-15 22:17:50 +0200

    Project training

--- LipraryMVPCourse/Program.cs
+++ LipraryMVPCourse/Program.cs
@@ -19,7 +19,7 @@ namespace LipraryMVPCourse
             Application.SetCompatibleTextRenderingDefault(false);
 
             BonusSkins.Register();
-            Application.Run(new Form1());
+            Application.Run(new Views.Frm_Main());
         }
     }
 }
---
+++ LipraryMVPCourse/Views/Frm_Main.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace LipraryMVPCourse.Views
+{
+    partial class Frm_Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Frm_Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(829, 416);
+            this.Name = "Frm_Main";
+            this.Text = "Frm_Main";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
